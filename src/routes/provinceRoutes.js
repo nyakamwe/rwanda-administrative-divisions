46128f25790046385e8a7c56ab6b569116eda763
nyakamwe <nyakamweaimable@gmail.com>
2022-06-08 11:56:57 +0200

    perform some logics and routes with full swagger documentaion of an endpoints

--- src/routes/provinceRoutes.js
+++ src/routes/provinceRoutes.js
@@ -2,7 +2,7 @@ import express from 'express'
 import { addProvince, editProvince, getAllProvinces, getSingleProvince } from '../controllers/provinceControllers'
 const router = express.Router()
 
-router.post('/provinces', addProvince)
+router.post('/province', addProvince)
 router.put('/provinces/:provinceUuid', editProvince)
 router.get('/provinces', getAllProvinces)
 router.get('/provinces/:provinceUuid', getSingleProvince)
